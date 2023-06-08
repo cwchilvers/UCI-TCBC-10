71a7dfdda6941b65b1e081b21129b79615e7807c
Chandler Chilvers <cwchilvers@gmail.com>
2023-06-08 13:06:52 -0700

    fixed createShape

--- index.js
+++ index.js
@@ -38,6 +38,9 @@ inquirer
         console.log('\x1b[34m%s\x1b[0m', 'Creating logo.svg...');
         const logoShape = createShape(data.shape, data.shapeColor);
         const logoText = createText(data.text, data.textColor);
+
+
+        console.log(logoShape);
         console.log(logoText);
 
 
@@ -45,8 +48,8 @@ inquirer
         //const logoSVG = createSVG(logoShape, logoText);
 
         // Create SVG file
-        //fs.writeFile('./output/README.md', markdown, (err) =>
+        //fs.writeFile('./output/logo.svg', logoSVG, (err) =>
             // Inform user if there was an error or else inform user if SVG file was successfully created
-            //err ? console.error('\x1b[31m%s\x1b[0m', err) : console.log('\x1b[42m%s\x1b[0m', 'Successfully created logo.svg. File can be found inside the output folder')
+            //err ? console.error('\x1b[31m%s\x1b[0m', err) : console.log('\x1b[42m%s\x1b[0m', 'Success! Logo.svg file can be found inside the output folder.')
         //);
     });
