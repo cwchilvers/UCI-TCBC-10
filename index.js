f75bbc7b1104930e713bf7b0440a178834ad2ca7
Chandler Chilvers <cwchilvers@gmail.com>
2023-06-08 15:37:15 -0700

    Moved inquirer and writeFile out of index.js into separate scripts. Created an asynchronous main function in index.js.

--- index.js
+++ index.js
@@ -1,49 +1,30 @@
 // Import modules
-const inquirer = require('inquirer');
-const fs = require('fs');
+const inquire = require("./lib/inquirer.js")
 const createShape = require("./lib/shapes.js");
 const createText = require("./lib/text.js");
 const createSVG = require("./lib/svg.js");
+const writeFile = require("./lib/writeFile.js");
 
-// Ask user for information
-inquirer 
-    .prompt([
-        {
-            type: 'input',
-            message: 'Logo Text (3 Characters Max):',
-            name: 'text',
-        },
-        {
-            type: 'input',
-            message: 'Logo Text Color:',
-            name: 'textColor',
-        },
-        {
-            type: 'list',
-            message: 'Logo Shape:',
-            name: 'shape',
-            choices: [
-                '● Circle',
-                '■ Square',
-                '▲ Triangle',
-            ],
-        },
-        {
-            type: 'input',
-            message: 'Logo Shape Color:',
-            name: 'shapeColor',
-        }
-    ])
+// Wrap everything in an asyncronous function so everything else executes after inquirer is finished
+async function main() {
+    // User input (wait for inquirer to finish before moving on)
+    const data = await inquire();
+    //Create logo
+    createLogo(data);
+}
 
-    .then((data) => {
-        console.log('\x1b[44m%s\x1b[0m', '\nCreating logo.svg...\n');
-        const logoShape = createShape(data.shape, data.shapeColor);
-        const logoText = createText(data.text, data.textColor);
-        const logoSVG = createSVG(logoShape, logoText)
+function createLogo(data) {
+    // Inform user that logo.svg is being created
+    console.log('\x1b[44m%s\x1b[0m', '\nCreating logo.svg...\n');
 
-        // Create SVG file
-        fs.writeFile('./output/logo.svg', logoSVG, (err) =>
-            // Inform user if there was an error or else inform user if SVG file was successfully created
-            err ? console.error('\x1b[41m%s\x1b[0m', err) : console.log('\x1b[32m%s\x1b[0m', 'Success! Logo.svg file can be found inside the output folder.')
-        );
-    });
+    // Create elements for SVG file
+    const logoShape = createShape(data.shape, data.shapeColor);
+    const logoText = createText(data.text, data.textColor);
+    const logoSVG = createSVG(logoShape, logoText)   
+
+    // Write to SVG file
+    writeFile(logoSVG);
+}
+
+// Run main
+main();
