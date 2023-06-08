106935ea8301fb60f21c38befb8b57f98237f92c
Chandler Chilvers <cwchilvers@gmail.com>
2023-06-08 13:42:26 -0700

    Added a function to create the SVG content and another function to write to an .svg file

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 const createShape = require("./lib/shapes.js");
 const createText = require("./lib/text.js");
+const createSVG = require("./lib/svg.js");
 
 // Ask user for information
 inquirer 
@@ -35,21 +36,14 @@ inquirer
     ])
 
     .then((data) => {
-        console.log('\x1b[34m%s\x1b[0m', 'Creating logo.svg...');
+        console.log('\x1b[44m%s\x1b[0m', '\nCreating logo.svg...\n');
         const logoShape = createShape(data.shape, data.shapeColor);
         const logoText = createText(data.text, data.textColor);
-
-
-        console.log(logoShape);
-        console.log(logoText);
-
-
-
-        //const logoSVG = createSVG(logoShape, logoText);
+        const logoSVG = createSVG(logoShape, logoText)
 
         // Create SVG file
-        //fs.writeFile('./output/logo.svg', logoSVG, (err) =>
+        fs.writeFile('./output/logo.svg', logoSVG, (err) =>
             // Inform user if there was an error or else inform user if SVG file was successfully created
-            //err ? console.error('\x1b[31m%s\x1b[0m', err) : console.log('\x1b[42m%s\x1b[0m', 'Success! Logo.svg file can be found inside the output folder.')
-        //);
+            err ? console.error('\x1b[41m%s\x1b[0m', err) : console.log('\x1b[32m%s\x1b[0m', 'Success! Logo.svg file can be found inside the output folder.')
+        );
     });
