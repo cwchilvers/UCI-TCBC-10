c64731efff37dfbd77e88a3039ffca82660d6ea6
Chandler Chilvers <cwchilvers@gmail.com>
2023-06-08 18:44:09 -0700

    Created test for testing shape creation

---
+++ tests/shapes.test.js
@@ -0,0 +1,25 @@
+const createShape = require("./lib/shapes.js");
+
+// Test creating shapes
+describe("Shapes", () => {
+    test("should create the correct shape with the correct color" , () => {
+        // input to use for test
+        const input = [
+            {shape: '● Circle', color: 'blue'},
+            {shape: '■ Square', color: 'orange'},
+            {shape: '▲ Triangle', color: 'purple'}           
+        ]
+
+        // correct output to compare to
+        const output = [
+            "<circle cx='150' cy='100' r='80' fill='blue'/>",
+            "<rect x='70' y='20' width='160' height='160' fill='black'/>",
+            "<polygon points='150, 18 244, 182 56, 182' fill='orange'/>"                
+        ]
+
+        // compare input to output to see if correct
+        for (const i of input) {
+            expect(createShape(input[i].shape, input[i].color)).toEqual(output[i]);
+        }
+    })
+});
