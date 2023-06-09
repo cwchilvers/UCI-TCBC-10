15ec7820c6cc7ba5adb8444c67f8836282a40765
Chandler Chilvers <cwchilvers@gmail.com>
2023-06-08 21:25:28 -0700

    Updated shapes test

--- tests/shapes.test.js
+++ tests/shapes.test.js
@@ -1,25 +1,28 @@
 const createShape = require("../lib/shapes.js");
 
+// input to use for test
+const input = [
+    {shape: '● Circle', color: 'blue'},
+    {shape: '■ Square', color: 'orange'},
+    {shape: '▲ Triangle', color: 'purple'}           
+]
+
+// correct output to compare input to
+const output = [
+    "<circle cx='150' cy='100' r='80' fill='blue'/>",
+    "<rect x='70' y='20' width='160' height='160' fill='orange'/>",
+    "<polygon points='150, 18 244, 182 56, 182' fill='purple'/>"                
+]
+
 // Test creating shapes
 describe("Shapes", () => {
-    test("should create the correct shape with the correct color" , () => {
-        // input to use for test
-        const input = [
-            {shape: '● Circle', color: 'blue'},
-            {shape: '■ Square', color: 'orange'},
-            {shape: '▲ Triangle', color: 'purple'}           
-        ]
-
-        // correct output to compare to
-        const output = [
-            "<circle cx='150' cy='100' r='80' fill='blue'/>",
-            "<rect x='70' y='20' width='160' height='160' fill='black'/>",
-            "<polygon points='150, 18 244, 182 56, 182' fill='orange'/>"                
-        ]
-
-        // compare input to output to see if correct
-        for (const i of input) {
-            expect(createShape(input.shape, input.color)).toEqual(output[i]);
-        }
+    test("should create a blue circle" , () => {
+        expect(createShape(input[0].shape, input[0].color)).toEqual(output[0]);
+    })
+    test("should create an orange square" , () => {
+        expect(createShape(input[1].shape, input[1].color)).toEqual(output[1]);
+    })
+    test("should create a purple triangle" , () => {
+        expect(createShape(input[2].shape, input[2].color)).toEqual(output[2]);
     })
 });
